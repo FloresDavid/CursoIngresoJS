6cf28f09719c7e37d88d1c1be52df7afb5cb5b50
FloresDavid <36281512+FloresDavid@users.noreply.github.com>
2018-02-08 20:39:51 -0300

    Ejercicio 1 y 2
    alert, prompt, variables, valores literales, indefinido

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,16 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	//Defino una variable
+	var nombre;
+	//alert (nombre);
+	nombre = prompt("ingrese su nombre", "natalia natalia");
+	alert (nombre);
+	/*
+	nombre= "David";
+	alert(nombre);
+	alert("nombre")
+	*/
+	//var person = prompt("Please enter your name", "Harry Potter");
 }
 
