c0da703740b061b98b55da51d5dbff3a17a9b6b4
FloresDavid <36281512+FloresDavid@users.noreply.github.com>
2018-02-09 19:41:05 -0300

    Ejercicios 7, 8, 9 y 10
    Operaciones suma, resta, division, multiplicacion, resto, añadir y restar porcentajes

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,11 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	var importe;
+	var resultado;
+
+	importe = parseInt(document.getElementById('importe').value);
+	resultado = importe - (importe/4);
+	document.getElementById('resultado').value = resultado
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,51 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
+
+	var uno;
+	var dos;
+	var suma;
+	var resta;
+	var multiplicacion;
+	var division;
+
 function sumar()
-{	
-	
+{
+	uno=parseInt(document.getElementById('numeroUno').value);
+	dos=parseInt(document.getElementById('numeroDos').value);
+
+	suma = uno + dos;
+
+	alert ("La suma es " + suma);
 }
 
 function restar()
 {
-	
+	uno=parseInt(document.getElementById('numeroUno').value);
+	dos=parseInt(document.getElementById('numeroDos').value);
+
+	resta = uno - dos;
+
+	alert("La resta es " + resta);
 }
 
 function multiplicar()
 { 
-	
+	uno=parseInt(document.getElementById('numeroUno').value);
+	dos=parseInt(document.getElementById('numeroDos').value);
+
+	multiplicacion = uno * dos;
+
+	alert("La multiplicacion es " + multiplicacion);
 }
 
 function dividir()
 {
-	
-}
+	uno=parseInt(document.getElementById('numeroUno').value);
+	dos=parseInt(document.getElementById('numeroDos').value);
 
+	division = uno / dos;
+
+	alert("La division es " + division); 
+}
