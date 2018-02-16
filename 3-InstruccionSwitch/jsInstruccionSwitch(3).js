b17c3716f31efebe7b9f622d4e2a8a8cd1fb3314
FloresDavid <36281512+FloresDavid@users.noreply.github.com>
2018-02-15 22:17:30 -0300

    Ejercicios del 1 al 4
    Instrucción switch usando case, break y default

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,19 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	//tomo la edad  
+	var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+	//alert (mesDelAño);
+	switch(mesDelAño) {
+
+
+		case "Febrero":
+			alert ("Este mes no tiene mas de 29 dias.");
+			break;
+
+		default:
+			alert ("Este mes tiene 30 o más dias.")
+	}		
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,11 +1,29 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	//tomo la edad  
+	var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+	//alert (mesDelAño);
+	switch(mesDelAño) {
+		case "Febrero":
+			alert ("Este mes tiene 28 días (29 en años biciestos)");
+			break
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":	
+		case "Diciembre":
+			alert ("Este mes tiene 31 días");
+			break
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert ("Este mes tiene 30 días");
+	}
+		
 
 
 
